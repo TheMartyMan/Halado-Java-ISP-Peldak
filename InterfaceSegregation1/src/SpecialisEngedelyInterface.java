a36ee93104cbfe8f3f80fd66d05f8dd1e2baa2ba
GMárton <martonstar@gmail.com>
2023-06-13 10:43:02 +0200

    Update SpecialisEngedelyInterface.java

--- InterfaceSegregation1/src/SpecialisEngedelyInterface.java
+++ InterfaceSegregation1/src/SpecialisEngedelyInterface.java
@@ -1,11 +1,3 @@
-// Ez az interfész tartalmazza a grantSpecialEngedely, és a revokeSpecialEngedely metódusokat,
-// amelyeket az implementáló osztályoknak meg kell valósítaniuk.
-
-// Nem tartalmaz olyan metódust, amit nem használ a speciális engedély osztály, és
-// ezekhez a metódusokhoz az alap engedélyekre jogosult osztály nem fér hozzá
-
-
-
 public interface SpecialisEngedelyInterface {
 
     void grantSpecialEngedely();
