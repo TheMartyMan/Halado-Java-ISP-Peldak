1b79117bf4947395fe8063a44c05082af12b77d7
GMárton <martonstar@gmail.com>
2023-06-13 10:44:02 +0200

    Update Repulo.java

--- InterfaceSegregation2/src/Repulo.java
+++ InterfaceSegregation2/src/Repulo.java
@@ -1,5 +1,3 @@
-// A repülő egy olyan jármű, ami mozgásra, emellett repülésre is képes, ennek megfelelően implementálja az interfaceket
-
 public class Repulo implements Jarmu, Mozgo, Reptetheto {
 
     @Override
