f65f3e07b5e3ddd0537a1ead009b1e2d6c66e17b
GMárton <martonstar@gmail.com>
2023-06-13 10:42:17 +0200

    Update AlapEngedely.java

--- InterfaceSegregation1/src/AlapEngedely.java
+++ InterfaceSegregation1/src/AlapEngedely.java
@@ -1,17 +1,13 @@
-// Csak az alap engedélyeket éri el
-
 class AlapEngedely implements AlapEngedelyInterface {
 
     @Override
     public void grantAlapEngedely() {
-        // Alap engedély megadása
         System.out.println("Az alapvető engedélyekre feljogosító jogosultság hozzáadva!\n");
     }
 
 
     @Override
     public void revokeAlapEngedely() {
-        // Alap engedély visszavonása
         System.out.println("Alap jogosultság visszavonva!\n");
     }
 }
