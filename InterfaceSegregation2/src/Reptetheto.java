5ae124bf622d6f0420905db50e149afcd4f72786
GMárton <martonstar@gmail.com>
2023-06-13 10:43:50 +0200

    Update Reptetheto.java

--- InterfaceSegregation2/src/Reptetheto.java
+++ InterfaceSegregation2/src/Reptetheto.java
@@ -1,10 +1,3 @@
-// Az interfacek külön-külön szerepelnek, más "jogokkal" bírnak, nem egyetlen nagy interfaceben van minden metódus.
-// Így minden jármű külön-külön kapja meg őket attól függően, hogy melyik szükséges, melyik tulajdonság/funkció igaz rájuk.
-
-
-// A repülni képes járművek tudnak repülni.
-
-
 public interface Reptetheto {
 
     void repul();
