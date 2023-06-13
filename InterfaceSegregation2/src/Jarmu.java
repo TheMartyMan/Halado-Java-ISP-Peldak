c311152242db321ce0d8ac7efeaad0cc422d9fbd
GMárton <martonstar@gmail.com>
2023-06-13 10:43:28 +0200

    Update Jarmu.java

--- InterfaceSegregation2/src/Jarmu.java
+++ InterfaceSegregation2/src/Jarmu.java
@@ -1,9 +1,3 @@
-// Az interfacek külön-külön szerepelnek, más "jogokkal" bírnak, nem egyetlen nagy interfaceben van minden metódus.
-// Így minden jármű külön-külön kapja meg őket attól függően, hogy melyik szükséges, melyik tulajdonság/funkció igaz rájuk.
-
-
-// Minden jármű rendelkezik árral, és színnel.
-
 public interface Jarmu {
 
         void setAr(double ar);
